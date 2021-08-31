87a188366666a4a3e5d0250e493e00d0b8e258c7
0975529840 <vuminhtuan03062000@gmail.com>
2021-09-01 01:36:43 +0700

    modify

--- ESPInsertDB.php
+++ ESPInsertDB.php
@@ -6,9 +6,9 @@
     if($connection->connect_error){
         die("Connection failed: " . $conn->connect_error);
     }
-    $date = date("Y-m-d");
+    $date = date("Y-m-d H:M:s");
     $time = date("H:i:s");
-    $sql="INSERT INTO sensor (ND, DA, BTcheck,date,time) VALUES ('".$ND."','".$DA."','".$BTcheck."','".$date."','".$time."')";
+    $sql="INSERT INTO sensor (ND, DA, BTcheck,date) VALUES ('".$ND."','".$DA."','".$BTcheck."','".$date."')";
     if($connection->query($sql)){
         echo "Insert OK";
     }else{
